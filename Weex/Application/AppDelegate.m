5b9a4ce9473fb86a92b4162e1c461298fec66a6c
zhangsr <zhangsr@tiaohuo.com>
2018-05-10 23:19:57 +0800

    3.2.1

--- Weex/Application/AppDelegate.m
+++ Weex/Application/AppDelegate.m
@@ -61,7 +61,7 @@
     [SVProgressHUD setMaximumDismissTimeInterval:0.5];
     [SVProgressHUD setMaxSupportedWindowLevel:UIWindowLevelAlert + 1000];
     
-    [[NSUserDefaults standardUserDefaults] setObject:@"zh-Hans" forKey:@"appLanguage"];
+    [[NSUserDefaults standardUserDefaults] setObject:@"zh-Hans" forKey:@"preferredLanguages"];
 //    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 //    _live = [[CJLivePushViewController alloc] init];
 //    self.window.rootViewController = _live;
