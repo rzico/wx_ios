42b17c0271fbf50675b4d570be184bb27af656a1
zhangsr <zhangsr@tiaohuo.com>
2018-05-19 16:18:22 +0800

    3.2.1

--- Settings.h
+++ Settings.h
@@ -42,7 +42,7 @@ static NSString *WXCONFIG_COLOR = @"#F0AD3C";
 static NSString *WXCONFIG_RESOURCE_PATH = @"http://cdnx.rzico.com/";
 
 //接口地址
-static NSString *WXCONFIG_INTERFACE_PATH = @"http://dev.rzico.com/";
+static NSString *WXCONFIG_INTERFACE_PATH = @"http://water.rzico.com/";
 
 //微信参数
 static NSString *kAuthScope = @"snsapi_message,snsapi_userinfo,snsapi_friend,snsapi_contact";
